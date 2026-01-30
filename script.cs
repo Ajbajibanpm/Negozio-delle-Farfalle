66a20fcf98e7128b0625263fa336fd3f6cc9a04e
Ajbajibanpm <elenadivino@gmail.com>
2026-01-30 11:15:43 +0100

    Delete script.cs

--- script.cs
+++
@@ -1,7 +0,0 @@
-<script>
-  document.querySelector('a[href="#home"]').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    history.replaceState(null, null, ' '); // Rimuove il # dall'URL
-  });
-</script>
